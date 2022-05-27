70316d723631187b853395377ec58b3101dd2865
JyrpoKoo <13076652356@163.com>
2022-05-27 09:02:16 +0800

    优化

---
+++ src/components/iconpark/c-icon.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'c-icon',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    theme: {
+      type: String,
+      default: 'outline'
+    },
+    size: {
+      type: [Number, String],
+      default: 24
+    },
+    fill: {
+      type: String,
+      default: '#ccc'
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  /*   render: function (createElement) {
+    return createElement('h1', '23232')
+  } */
+  render: function (h) {
+    const {name, theme, size, fill} = this.$props
+    console.log(this.$props)
+    const icon = {
+      template: `<ipark-${name} theme="${theme}" size="${size}" fill="${fill}"></ipark-home>`
+    }
+    return h(icon)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
